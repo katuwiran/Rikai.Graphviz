8ca8c6e96e1b80036619ccf02523a1dae30698e9
K. Rikaisuru <shirurururu@outlook.com>
2026-02-11 22:43:47 +0800

    Added support for labels only. Fixed some logic.

--- Rikai.Graphviz/Types/Edge.cs
+++ Rikai.Graphviz/Types/Edge.cs
@@ -2,11 +2,11 @@ namespace Rikai.Graphviz;
 
 public class Edge
 {
-	public List<string>? FromNodeIds { get; set; }
-	public List<string>? ToNodeIds   { get; set; }
+	public List<string?> FromNodeIds { get; set; } = new();
+	public List<string?> ToNodeIds   { get; set; } = new();
 
 	/// <summary>
-	/// Determines the starting point of this Edge
+	/// Determines the starting point of this Edge. Uses predefined nodes for Ids.
 	/// </summary>
 	/// <param name="node"></param>
 	/// <returns>Edge</returns>
@@ -17,18 +17,18 @@ public class Edge
 	}
 
 	/// <summary>
-	/// Determines the starting point of this Edge
+	/// Determines the starting point of this Edge. Uses predefined nodes for Ids.
 	/// </summary>
-	/// <param name="node"></param>
+	/// <param name="nodes"></param>
 	/// <returns>Edge</returns>
 	public Edge From(IEnumerable<Node> nodes)
 	{
-		FromNodeIds = nodes.Select(n => n.Id).ToList();
+		FromNodeIds.AddRange(nodes.Select(n => n.Id).ToList());
 		return this;
 	}
 
 	/// <summary>
-	/// Determines the end point of this Edge
+	/// Determines the end point of this Edge. Uses predefined nodes for Ids.
 	/// </summary>
 	/// <param name="node"></param>
 	public void To(Node node)
@@ -37,13 +37,54 @@ public class Edge
 	}
 
 	/// <summary>
-	/// Determines the starting point of this Edge
+	/// Determines the starting point of this Edge. Uses predefined nodes for Ids.
 	/// </summary>
-	/// <param name="node"></param>
+	/// <param name="nodes"></param>
 	public void To(IEnumerable<Node> nodes)
 	{
-		ToNodeIds = nodes.Select(n => n.Id).ToList();
+		ToNodeIds.AddRange(nodes.Select(n => n.Id).ToList());
 	}
+
+	/// <summary>
+	/// Determines the starting point of this Edge. Uses labels only for reference.
+	/// </summary>
+	/// <param name="label"></param>
+	/// <returns>Edge</returns>
+	public Edge From(String label)
+	{
+		FromNodeIds.Add(label);
+		return this;
+	}
+
+	/// <summary>
+	/// Determines the starting point of this Edge. Uses labels only for reference.
+	/// </summary>
+	/// <param name="labels"></param>
+	/// <returns>Edge</returns>
+	public Edge From(IEnumerable<String> labels)
+	{
+		FromNodeIds.AddRange(labels);
+		return this;
+	}
+
+	/// <summary>
+	/// Determines the end point of this Edge. Uses labels only for reference.
+	/// </summary>
+	/// <param name="label"></param>
+	public void To(String label)
+	{
+		ToNodeIds.Add(label);
+	}
+
+	/// <summary>
+	/// Determines the starting point of this Edge. Uses labels only for reference.
+	/// </summary>
+	/// <param name="labels"></param>
+	public void To(IEnumerable<String> labels)
+	{
+		ToNodeIds.AddRange(labels);
+	}
+
 
 	public List<Edge> ToConsecutively()
 	{
