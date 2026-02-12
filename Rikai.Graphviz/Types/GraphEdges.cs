5c424889f9415aafcc07e821de7d11bf6bf2a47a
K. Rikaisuru <shirurururu@outlook.com>
2026-02-12 14:48:53 +0800

    feat: added logic to graph edges

--- Rikai.Graphviz/Types/GraphEdges.cs
+++ Rikai.Graphviz/Types/GraphEdges.cs
@@ -2,5 +2,31 @@ namespace Rikai.Graphviz;
 
 public class GraphEdges
 {
-	public EdgeAttributes Attributes { get; set; } = new();
+	public   EdgeAttributes Attributes { get; set; }  = new();
+	public   List<Edge>     Edges      { get; init; } = new();
+	internal GraphNodes     Nodes; // constructor DI
+
+	public GraphEdges(Graph graph)
+	{
+		Nodes = graph.Nodes;
+	}
+
+	// Add one edge
+	public void Add(Edge edge)
+	{
+		Edges.Add(edge);
+		Nodes.AddNodesToCollectionIfNotExists(edge.FromNodes);
+		Nodes.AddNodesToCollectionIfNotExists(edge.ToNodes);
+	}
+
+	// Add edges
+	public void Add(IEnumerable<Edge> edges)
+	{
+		Edges.AddRange(edges);
+		foreach (var edge in edges)
+		{
+			Nodes.AddNodesToCollectionIfNotExists(edge.FromNodes);
+			Nodes.AddNodesToCollectionIfNotExists(edge.ToNodes);
+		}
+	}
 }
