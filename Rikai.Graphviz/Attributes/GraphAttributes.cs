759236ccf86b439cd4add27790a90abfc8d877ee
K. Rikaisuru <shirurururu@outlook.com>
2026-02-12 15:35:38 +0800

    feat: added graph attribute definitions

--- Rikai.Graphviz/Attributes/GraphAttributes.cs
+++ Rikai.Graphviz/Attributes/GraphAttributes.cs
@@ -2,9 +2,13 @@ namespace Rikai.Graphviz;
 
 public class GraphAttributes
 {
-	public class Attribute
-	{
-		public string Title    { get; set; }
-		public string FontName { get; set; }
-	}
+	public string? Label           { get; set; }
+	public string? FontColor       { get; set; }
+	public string? FontName        { get; set; }
+	public string? BackgroundColor { get; set; }
+
+	public Splines? Splines { get; set; }
+	public RankDir? RankDir { get; set; }
+
+	public LayoutEngine? LayoutEngine { get; set; }
 }
