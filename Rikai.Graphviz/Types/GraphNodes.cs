ea1820d58c6c82e50ad7714fe21c1ee61dd1668e
K. Rikaisuru <shirurururu@outlook.com>
2026-02-12 14:48:18 +0800

    feat: added logic to graph nodes

--- Rikai.Graphviz/Types/GraphNodes.cs
+++ Rikai.Graphviz/Types/GraphNodes.cs
@@ -3,4 +3,70 @@ namespace Rikai.Graphviz;
 public class GraphNodes
 {
 	public NodeAttributes Attributes { get; set; } = new();
+
+	internal List<Node>   Nodes   { get; set; } = new();
+	internal List<string> NodeIds { get; set; } = new();
+	
+	public GraphNodes Add(Node node)
+	{
+		AddNodeToCollectionIfNotExists(node);
+		return this;
+	}
+
+	public void Add(IEnumerable<Node> nodes)
+	{
+		foreach (var node in nodes)
+		{
+			AddNodeToCollectionIfNotExists(node);
+		}
+	}
+
+	public void Add(string label)
+	{
+		var node = new Node(label);
+		AddNodeToCollectionIfNotExists(node);
+	}
+
+	public void Add(params string[] labels)
+	{
+		foreach (var label in labels)
+		{
+			var node = new Node(label);
+			AddNodeToCollectionIfNotExists(node);
+		}
+	}
+	
+	internal void AddNodesToCollectionIfNotExists(params Node[] nodes)
+	{
+		foreach (var node in nodes)
+		{
+			var id = node.Id;
+			if (!NodeIds.Contains(id))
+			{
+				Nodes.Add(node);
+				NodeIds.Add(node.Id);
+			}
+		}
+	}
+
+	internal void AddNodeToCollectionIfNotExists(Node node)
+	{
+		if (Nodes.Contains(node))
+		{
+			Nodes.Add(node);
+			NodeIds.Add(node.Id);
+		}
+	}
+
+	internal void AddNodesToCollectionIfNotExists(IEnumerable<Node> nodes)
+	{
+		foreach (var node in nodes)
+		{
+			if (Nodes.Contains(node))
+			{
+				Nodes.Add(node);
+				NodeIds.Add(node.Id);
+			}
+		}
+	}
 }
