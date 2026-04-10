432c1eccb0a57c9be888319bd946c4ad1c9f0079
K. Rikaisuru <shirurururu@outlook.com>
2026-04-10 19:13:50 +0800

    feat: node attributes now included in ctor

--- Rikai.Graphviz/Types/Node.cs
+++ Rikai.Graphviz/Types/Node.cs
@@ -2,18 +2,20 @@ namespace Rikai.Graphviz;
 
 public class Node
 {
-	public NodeAttributes Attributes { get; set; } = new();
+	public NodeAttributes Attributes { get; set; }
 	public string         Id         { get; set; }
 	public string?        Label      { get; set; }
 
-	public Node(string id)
+	public Node(string id, NodeAttributes? attributes = null)
 	{
 		Id = id;
+		Attributes = attributes ?? new NodeAttributes();
 	}
 
-	public Node(string id, string label)
+	public Node(string id, string label, NodeAttributes? attributes = null)
 	{
 		Id    = id;
 		Label = label;
+		Attributes = attributes ?? new NodeAttributes();
 	}
 }
