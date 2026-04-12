31bfc08a6496917f4f3b7399543059ba006368bb
K. Rikaisuru <shirurururu@outlook.com>
2026-04-12 22:28:22 +0800

    feat(builders): htmlCell, htmlTable attributes buiulder

---
+++ Rikai.Graphviz/Builders/Html/Attributes/HtmlCellAttributesBuilder.cs
@@ -0,0 +1,26 @@
+namespace Rikai.Graphviz.Builders;
+
+public class HtmlCellAttributeBuilder
+{
+	private readonly HtmlCellAttributes _attr;
+
+	public HtmlCellAttributeBuilder(HtmlCellAttributes attr) => _attr = attr;
+
+	// @formatter:off
+	public HtmlCellAttributeBuilder Align(HtmlAlign value) { _attr.Align       = value; return this; }
+	public HtmlCellAttributeBuilder BgColor(string  value) { _attr.BgColor     = value; return this; }
+	public HtmlCellAttributeBuilder Border(int      value) { _attr.Border      = value; return this; }
+	public HtmlCellAttributeBuilder CellBorder(int  value) { _attr.CellBorder  = value; return this; }
+	public HtmlCellAttributeBuilder CellPadding(int value) { _attr.CellPadding = value; return this; }
+	public HtmlCellAttributeBuilder CellSpacing(int value) { _attr.CellSpacing = value; return this; }
+	public HtmlCellAttributeBuilder Color(string    value) { _attr.Color       = value; return this; }
+	public HtmlCellAttributeBuilder ColSpan(int     value) { _attr.ColSpan     = value; return this; }
+	public HtmlCellAttributeBuilder Height(double   value) { _attr.Height      = value; return this; }
+	public HtmlCellAttributeBuilder Port(PortPos    value) { _attr.Port        = value; return this; }
+	public HtmlCellAttributeBuilder RowSpan(int     value) { _attr.RowSpan     = value; return this; }
+	public HtmlCellAttributeBuilder Style(HtmlStyle value) { _attr.Style       = value; return this; }
+	public HtmlCellAttributeBuilder Width(double    value) { _attr.Width       = value; return this; }
+	// @formatter:on 
+	
+	public HtmlCellAttributes Build() => _attr;
+}
---
+++ Rikai.Graphviz/Builders/Html/Attributes/HtmlTableAttributeBuilder.cs
@@ -0,0 +1,25 @@
+namespace Rikai.Graphviz.Builders;
+
+public class HtmlTableAttributeBuilder
+{
+	private readonly HtmlTableAttributes _attr;
+
+	public HtmlTableAttributeBuilder(HtmlTableAttributes attr) => _attr = attr;
+
+	// @formatter:off
+    public HtmlTableAttributeBuilder Align(HtmlAlign value) { _attr.Align       = value; return this; }
+    public HtmlTableAttributeBuilder BgColor(string value)  { _attr.BgColor     = value; return this; }
+    public HtmlTableAttributeBuilder TableBorder(int value) { _attr.Border      = value; return this; }
+    public HtmlTableAttributeBuilder CellBorder(int value)  { _attr.CellBorder  = value; return this; }
+    public HtmlTableAttributeBuilder CellPadding(int value) { _attr.CellPadding = value; return this; }
+    public HtmlTableAttributeBuilder CellSpacing(int value) { _attr.CellSpacing = value; return this; }
+    public HtmlTableAttributeBuilder Color(string value)    { _attr.Color       = value; return this; }
+    public HtmlTableAttributeBuilder Port(string value)     { _attr.Port        = value; return this; }
+	// @formatter:on
+
+    public HtmlTableAttributeBuilder Style(HtmlStyle value)
+    {
+        _attr.Style = value;
+        return this;
+    }
+}
