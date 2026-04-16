277fe529a174a7c4a1091cf93429ee73cedb5a15
K. Rikaisuru <shirurururu@outlook.com>
2026-04-16 23:36:07 +0800

    feat(types): the text of an html cell is now in its constructor

--- Rikai.Graphviz/Types/Html/HtmlCell.cs
+++ Rikai.Graphviz/Types/Html/HtmlCell.cs
@@ -3,10 +3,12 @@ namespace Rikai.Graphviz;
 public class HtmlCell
 {
 	public string             Id         { get; set; }
+	public string?            Text       { get; set; }
 	public HtmlCellAttributes Attributes { get; set; } = new();
 
-	public HtmlCell(string id)
+	public HtmlCell(string id,  string? text = null)
 	{
 		Id = id;
+		Text = text;
 	}
 }
