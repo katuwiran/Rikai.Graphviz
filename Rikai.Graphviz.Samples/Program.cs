15b8ae883fc0a7d18d1172fb7ed9d342df36c685
K. Rikaisuru <shirurururu@outlook.com>
2026-04-25 13:21:42 +0800

    feat(samples): there is now a simple menu to print and output a sample Graph

--- Rikai.Graphviz.Samples/Program.cs
+++ Rikai.Graphviz.Samples/Program.cs
@@ -4,16 +4,45 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine();
-		// Console.WriteLine(Graphs.Fluent());
-		// Console.WriteLine();
-		// Console.WriteLine(Graphs.Syntax());
-		// Console.WriteLine();
-		Console.WriteLine(Graphs.LibraryStructure());
-		// Console.WriteLine();
-		// Console.WriteLine(Graphs.Meta);
-		// Console.WriteLine();
-		// Console.WriteLine(Graphs.ReadMe());
-		// Console.WriteLine();
+		string choice;
+		int    choiceInt;
+
+		Graph graph;
+
+		while (true)
+		{
+			Console.WriteLine("Press a number to choose a graph");
+			Console.WriteLine("1. Fluent");
+			Console.WriteLine("2. Library Structure");
+			Console.WriteLine("3. Syntax");
+			Console.WriteLine("4. Readme");
+			Console.WriteLine("5. Meta");
+			Console.Write("Choose a number: ");
+
+			choice = Console.ReadKey().KeyChar.ToString();
+
+			if (Int32.TryParse(choice, out choiceInt))
+			{
+				switch (choiceInt)
+				{
+					case 1: graph = Graphs.Fluent(); break;
+					case 2: graph = Graphs.LibraryStructure(); break;
+					case 3: graph = Graphs.Syntax(); break;
+					case 4: graph = Graphs.ReadMe(); break;
+					case 5: graph = Graphs.Meta(); break;
+					default:
+						graph = Graphs.Mikotoba(); 
+						break;
+				}
+
+				Console.WriteLine(graph);
+				File.WriteAllText("out.dot", graph.ToString());
+				break;
+			}
+			else
+			{
+				Console.WriteLine("Invalid input.");
+			}
+		}
 	}
 }
